2b2c225b4f390d64f479148c047f76292e5d9948
James <soapman@zoho.com>
2022-07-22 13:33:39 -0500

    Fri 07/22/22 13:33 - Monterey

---
+++ automator/vsc.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+
+sh -c "/Applications/VSCodium.app/Contents/Resources/app/bin/codium  --extensions-dir \$HOME/Library/Application\ Support/vscode/extensions --user-data-dir \$HOME/Library/Application\ Support/vscode/user-data"
+
