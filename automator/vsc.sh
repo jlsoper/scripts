9e4256424504fa46c7faf9e01cfbf7e29ec46e07
James <soapman@zoho.com>
2022-07-22 17:49:59 -0500

    Fri 07/22/22 17:49 - Monterey

--- automator/vsc.sh
+++ automator/vsc.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
 
+# OSX Dock -  Finder (icon view)
+# OSX      -  ./Contents/Resources/ApplicationStub.icns (overwrite icon file)
+
 # $HOME/scripts/automator/vsc.sh
 
 sh -c "/Applications/VSCodium.app/Contents/Resources/app/bin/codium  --extensions-dir \$HOME/Library/Application\ Support/vscode/extensions --user-data-dir \$HOME/Library/Application\ Support/vscode/user-data"
