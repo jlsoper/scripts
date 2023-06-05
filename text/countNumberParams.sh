e4886c27d620a6374e30c75c9fb035abb5fac5ba
James <soapman@zoho.com>
2023-06-05 13:09:53 -0500

    Mon 06/05/23 13:09 -- Leopard \ jsoper

---
+++ text/countNumberParams.sh
@@ -0,0 +1,37 @@
+#!/bin/ash
+
+
+: '
+//////////
+
+
+countNumberParams.sh "one two three" four five
+
+
+
+
+//////////
+'
+
+
+echo -n "Input your first name: " ; read fname
+echo $fname
+
+
+
+#######
+
+
+main()
+{
+  echo 'main sees' $# 'args'
+}
+
+main $*
+main $@
+
+
+main "$*"
+main "$@"
+
+
