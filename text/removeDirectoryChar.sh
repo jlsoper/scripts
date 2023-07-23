a19a4f8683f94e41339aa1289622d783d1143db9
James <soapman@zoho.com>
2023-07-22 21:17:01 -0500

    Sat 07/22/23 21:17 -- Monterey \ jsoper

---
+++ text/removeDirectoryChar.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+
+####################################
+#
+#
+#  read -r  # Backslash does not act as an escape character
+#
+#
+####################################
+
+
+echo
+echo
+
+echo -n "Enter string with '/' or '\' : " ; read -r s
+echo
+
+
+if [ -n "$s" ]
+then
+  echo "The string you entered is: " "$s"
+  echo
+
+  echo ${s//\/};  #  /
+  echo ${s//\\};  #  \
+
+  echo
+  echo $s | tr -d '\/\\'
+
+fi
+
+
