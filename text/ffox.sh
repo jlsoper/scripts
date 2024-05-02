e06aecbabec81f1ff2762428d1452260d275d3a3
James <soapman22@frontier.com>
2024-05-02 13:45:18 -0500

    Thu 05/02/24 13:45 -- OSX \ jsoper

---
+++ text/ffox.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+####################################
+#
+#  Launch Firefox from terminal
+#
+#
+#  chmod 755 firefox.AppImage
+#
+#  https://appimage.github.io/Firefox_ESR/
+#
+####################################
+
+ cd /opt/AppImage
+
+  ./firefox-esr-next-102.3.r20220912135840-x86_64.AppImage &
+
+ cd
+
